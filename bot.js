57a7a6be59f41f8246f6d8bf2ec5f2563a1c1f44
fix-team <35807908+fix-team@users.noreply.github.com>
2018-12-23 12:52:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,25 @@ const Discord = require('discord.js');
 const robot = new Discord.Client();
 const prefix = "ft ";
 robot.login(process.env.BOT_TOKEN);
+robot.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === '/join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
+});
+
 robot.on('message', function(message) {
     if (message.author.equals(robot.user)) return;
     if (!message.content.startsWith(prefix)) return;
