a17924b0f34d939c65da21fa042f89913d086e76
Timofey <35807908+fix-team@users.noreply.github.com>
2022-09-07 16:12:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,8 +3,8 @@ const robot = new Discord.Client();
 const prefix = "";
 robot.login(process.env.BOT_TOKEN);
 robot.on('message', message => {
-  if (message.content === '/aljoin') {
-    message.reply('Past zatkni syka! FIRE');
+  if (message.content === 'разрешите') {
+    message.reply('Разрешаю');
     // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voiceChannel) {
       message.member.voiceChannel.join()
